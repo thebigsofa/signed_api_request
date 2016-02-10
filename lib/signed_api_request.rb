c6b5f305772ea8176b731d4023f6851578befdc7
KrzyczaK <krzyczak@gmail.com>
2016-02-10 12:15:35 +0100

    Renamed guid to uid

--- lib/signed_api_request.rb
+++ lib/signed_api_request.rb
@@ -8,36 +8,36 @@ module SignedApiRequest
     attr_accessor :encoder
 
     def configure
-      self.encoder ||= GuidsEncoder.new
+      self.encoder ||= UidsEncoder.new
       yield(encoder)
     end
 
-    def generate(guids:, expires:)
-      encoder.encode(guids: guids, expires: expires)
+    def generate(uids:, expires:)
+      encoder.encode(uids: uids, expires: expires)
     end
 
-    def validate(key_id:, secret:, guids:, expires:, signature:)
-      validating_encoder = GuidsEncoder.new
+    def validate(key_id:, secret:, uids:, expires:, signature:)
+      validating_encoder = UidsEncoder.new
       validating_encoder.key_id = key_id
       validating_encoder.secret = secret
-      signature_matched = (signature == CGI.unescape(validating_encoder.encode(guids: guids, expires: expires)))
+      signature_matched = (signature == CGI.unescape(validating_encoder.encode(uids: uids, expires: expires)))
       time_in_the_future = Time.at(expires.to_i).utc > Time.now.utc
       signature_matched && time_in_the_future
     end
   end
 
-  class GuidsEncoder
+  class UidsEncoder
     attr_accessor :key_id, :secret
 
-    def encode(guids:, expires:)
-      signature(expires.to_i, guids)
+    def encode(uids:, expires:)
+      signature(expires.to_i, uids)
     end
 
     private
 
-    def signature(expires, guids)
+    def signature(expires, uids)
       digest = OpenSSL::Digest.new('sha256')
-      hmac = OpenSSL::HMAC.digest(digest, @secret, "GET\n\n\n#{expires}\n/#{guids.join(',')}")
+      hmac = OpenSSL::HMAC.digest(digest, @secret, "GET\n\n\n#{expires}\n/#{uids.join(',')}")
       CGI.escape(URI.escape(Base64.encode64(hmac).strip))
     end
   end
